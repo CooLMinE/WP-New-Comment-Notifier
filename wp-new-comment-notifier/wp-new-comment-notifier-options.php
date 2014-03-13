0ef5a36ae3a690493d2e4604074b255345c19c0a
CooLMinE <CooLMinE@users.noreply.github.com>
2014-03-13 17:04:56 +0200

    Moved methods to a class with static methods. Removed unused files. Email headers will be requested only once per comment post event (before it used to trigger once per email that was about to be send). Renamed methods to remove the plugin name prefix. Updated version to 0.3.

--- wp-new-comment-notifier/wp-new-comment-notifier-options.php
+++ wp-new-comment-notifier/wp-new-comment-notifier-options.php
@@ -1,24 +1,22 @@
 <?php
-defined('ABSPATH') or die();    // Disallow direct access to file.
+// Disallow direct access to file.
+defined('ABSPATH') or die();
+?>    
 
-function wp_new_comment_notifier_options_page()
-{
-    ?>
-    <div class="wrap">
-        <h2>WP New Comment Notifier Options</h2>
-        <form method="post" action="options.php">
-    <?php settings_fields('WP_New_Comment_Notifier_Admin_Options'); ?>
-            <label><b>Email List</b> <small><i>(Seperate emails by commas)</i></small></label><br>
-            <textarea style='width:50%; resize:none; margin-bottom: 30px;' rows="5" wrap="off" cols='1' name="wp_new_comment_notifier_email_list"><?php echo get_option('wp_new_comment_notifier_email_list', get_option("admin_email")); ?></textarea><br>
+<!-- WP New Comment Notifer Options Start -->
+<div class="wrap">
+    <h2>WP New Comment Notifier Options</h2>
+    <form method="post" action="options.php">
+        <?php settings_fields('WP_New_Comment_Notifier_Admin_Options'); ?>
+        <label><b>Email List</b> <small><i>(Seperate emails by commas)</i></small></label><br>
+        <textarea style='width:50%; resize:none; margin-bottom: 30px;' rows="5" wrap="off" cols='1' name="wp_new_comment_notifier_email_list"><?php echo get_option('wp_new_comment_notifier_email_list', get_option("admin_email")); ?></textarea><br>
 
-            <label><b>Ignore Usernames</b> <small><i>(Seperate usernames by commas)</i></small></label><br>
-            <textarea style='width:50%; resize:none; margin-bottom: 30px;' rows="5" wrap="off" cols='1' name="wp_new_comment_notifier_username_ignore_list"><?php echo get_option('wp_new_comment_notifier_username_ignore_list', ""); ?></textarea><br>
+        <label><b>Ignore Usernames</b> <small><i>(Seperate usernames by commas)</i></small></label><br>
+        <textarea style='width:50%; resize:none; margin-bottom: 30px;' rows="5" wrap="off" cols='1' name="wp_new_comment_notifier_username_ignore_list"><?php echo get_option('wp_new_comment_notifier_username_ignore_list', ""); ?></textarea><br>
 
-            <label><b>Ignore IP Addresses</b> <small><i>(Seperate IP addresses by commas)</i></small></label><br>
-            <textarea style='width:50%; resize:none;' rows="5" wrap="off" cols='1' name="wp_new_comment_notifier_ip_ignore_list"><?php echo get_option('wp_new_comment_notifier_ip_ignore_list', ""); ?></textarea><br>
-    <?php submit_button(); ?>
-        </form>
-    </div>
-    <?php
-}
-?>
+        <label><b>Ignore IP Addresses</b> <small><i>(Seperate IP addresses by commas)</i></small></label><br>
+        <textarea style='width:50%; resize:none;' rows="5" wrap="off" cols='1' name="wp_new_comment_notifier_ip_ignore_list"><?php echo get_option('wp_new_comment_notifier_ip_ignore_list', ""); ?></textarea><br>
+        <?php submit_button(); ?>
+    </form>
+</div>
+<!-- WP New Comment Notifer Options End -->
--- wp-new-comment-notifier/wp-new-comment-notifier.php
+++ wp-new-comment-notifier/wp-new-comment-notifier.php
@@ -1,44 +1,154 @@
 <?php
-
 /*
   Plugin Name: WP New Comment Notifier
   Plugin URI:
   Description: A wordpress plugin that sends a notification to the emails of your choice when a new comment has been posted.
   Author: CooLMinE
   Author URI: http://www.fluxbytes.com/
-  Version: 0.1
+  Version: 0.3
  */
 
-defined('ABSPATH') or die();    // Disallow direct access to file.
+// Disallow direct access to file.
+defined('ABSPATH') or die();
 
-include "wp-new-comment-notifier-options.php";
-include "functions.php";
-
-add_action('comment_post', 'wp_ncn_on_new_comment_posted');
-
-function wp_ncn_on_new_comment_posted($comment_id)
+Class WP_New_Comment_Notifier
 {
-    $comment = get_comment($comment_id);
-    $subject = "New Comment by $comment->comment_author";
-    $emails = trim(get_option('wp_new_comment_notifier_email_list'));
-    $emailContent = wp_ncn_get_email_html_content($comment);
-
-    if (wp_ncn_is_user_ignored($comment->comment_author) == true || wp_ncn_is_ip_ignored($comment->comment_author_IP) == true)
+    public static function register_settings()
     {
-        return;
+        register_setting('WP_New_Comment_Notifier_Admin_Options', 'wp_new_comment_notifier_email_list');
+        register_setting('WP_New_Comment_Notifier_Admin_Options', 'wp_new_comment_notifier_username_ignore_list');
+        register_setting('WP_New_Comment_Notifier_Admin_Options', 'wp_new_comment_notifier_ip_ignore_list');
     }
 
-    if ($comment->comment_approved == "spam")
+    public static function register_option_page()
     {
-        $subject.= " (SPAM)";
+        add_submenu_page("options-general.php", "WP New Comment Notifier", "WP New Comment Notifier", "manage_options", __FILE__, array(__CLASS__, "show_option_page"));
     }
 
-    if (strpos($emails, '@') !== false)
+    public static function show_option_page()
     {
-        foreach (explode(',', $emails) as $email)
+        include "wp-new-comment-notifier-options.php";
+    }
+
+    /*
+     * Returns an HTML formatted string with all necessary details about the comment that was posted.
+     */
+    public static function get_email_html_content($comment)
+    {
+        $commentDate = get_comment_date(get_option('date_format') . ' \a\t ' . 'g:i:sa', $comment->comment_ID);
+        $authorUrl = $comment->comment_author_url == "" ? "N/A" : $comment->comment_author_url;
+
+        $emailContent = '<html>'
+                . '<b>' . $comment->comment_author . ' (' . $comment->comment_author_IP . ')</b>' . '<br/>'
+                . '<b>Date:</b> ' . $commentDate . '<br/>'
+                . '<b>Email:</b> ' . $comment->comment_author_email . '<br/>'
+                . '<b>Url:</b> ' . $authorUrl . '<br/>'
+                . '<b>Posted at:</b> ' . get_permalink($comment->comment_post_ID) . '<br/><br/><br/>'
+                . $comment->comment_content
+                . '</html>';
+
+        return $emailContent;
+    }
+
+    /*
+     * Returns the headers that should be set before sending an email.
+     */
+    public static function get_email_headers()
+    {
+        $blog_name = get_bloginfo("name");
+        $blog_admin_email = get_option("admin_email");
+        $headers = array();
+        $headers[] = "MIME-Version: 1.0";
+        $headers[] = "Content-type: text/html; charset=iso-8859-1";
+        $headers[] = "From: " . $blog_name . " <" . $blog_admin_email . ">";
+        $headers[] = "Reply-To: " . $blog_name . " <" . $blog_admin_email . ">";
+        $headers[] = "Return-Path: " . $blog_name . " <" . $blog_admin_email . ">";
+        $headers[] = "X-Mailer: PHP/" . phpversion();
+        return implode("\r\n", $headers);
+    }
+
+    /*
+     * Checks if the username is in the ignore list.
+     * 
+     * Returns true if the username is in the ignore list, otherwise returns false.
+     */
+    public static function is_user_ignored($username)
+    {
+        $ignorelist = explode(',', get_option("wp_new_comment_notifier_username_ignore_list"));
+
+        foreach ($ignorelist as $listentry)
         {
-            mail($email, $subject, $emailContent, wp_ncn_get_email_headers());
+            $entry = trim($listentry);
+
+            if (strlen($entry) != 0 && strcasecmp($entry, $username) == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /*
+     * Checks if an IP is set to be ignored.
+     * 
+     * Returns true if the IP is in the ignore list, otherwise returns false.
+     */
+    public static function is_ip_ignored($ip)
+    {
+        $ignorelist = explode(',', get_option("wp_new_comment_notifier_ip_ignore_list"));
+
+        foreach ($ignorelist as $listentry)
+        {
+            $entry = trim($listentry);
+
+            if (strlen($entry) != 0 && strcmp($entry, $ip) == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /*
+     * Triggers when a new comment is posted.
+     * 
+     * The function checks if the user that posted or the user's IP in is the ignored list, 
+     * if the user should not be ignored then the function proceeds to send an email to all the emails addresses 
+     * that have been set in the plugin's options page. 
+     */
+    public static function on_new_comment_posted($comment_id)
+    {
+        $comment = get_comment($comment_id);
+        $subject = "New Comment by $comment->comment_author";
+        $emails = trim(get_option('wp_new_comment_notifier_email_list'));
+        $emailContent = WP_New_Comment_Notifier::get_email_html_content($comment);
+
+        if (WP_New_Comment_Notifier::is_user_ignored($comment->comment_author) == true || WP_New_Comment_Notifier::is_ip_ignored($comment->comment_author_IP) == true)
+        {
+            return;
+        }
+
+        if ($comment->comment_approved == "spam")
+        {
+            $subject.= " (SPAM)";
+        }
+
+        if (strpos($emails, '@') !== false)
+        {
+            $emailHeaders = WP_New_Comment_Notifier::get_email_headers();
+            foreach (explode(',', $emails) as $email)
+            {
+                mail($email, $subject, $emailContent, $emailHeaders);
+            }
         }
     }
+
 }
+
+
+add_action('admin_init', array('WP_New_Comment_Notifier', 'register_settings'));
+add_action('admin_menu', array('WP_New_Comment_Notifier', 'register_option_page'));
+add_action('comment_post', array('WP_New_Comment_Notifier', 'on_new_comment_posted'));
 ?>
